b92a85f95c716cf743ad8802ee5ae0bd32975a8a
AriZox <arizoxfr@gmail.com>
2022-04-30 19:11:33 +0200

    Screamer handler (shows a screamer on a form)

---
+++ EscapeGameRemake/src/Screamer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EscapeGameRemake.src
+{
+    public class Screamer
+    {
+        private static Form mainForm;
+
+        public static void Run(Form mainForm, Path.Name name)
+        {
+            Screamer.mainForm = mainForm;
+            var screamer = new AxWMPLib.AxWindowsMediaPlayer();
+            mainForm.Controls.Add(screamer);
+            screamer.BringToFront();
+            screamer.PlayStateChange += Screamer_PlayStateChange;
+            screamer.CreateControl();
+            screamer.Visible = false;
+            screamer.URL = Path.Get(name);
+        }
+
+        private static void Screamer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            var screamer = (AxWMPLib.AxWindowsMediaPlayer)sender;
+
+            switch (e.newState)
+            {
+                case 1:
+                    //screamer.fullScreen = false;
+                    screamer.Visible = false;
+                    break;
+                case 3:
+                    screamer.Location = new System.Drawing.Point(0, 0);
+                    screamer.Width = mainForm.Width;
+                    screamer.Height = mainForm.Height;
+                    screamer.uiMode = "none";
+                    //screamer.fullScreen = true;
+                    screamer.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+                    screamer.Visible = true;
+                    break;
+                /*default:
+                    Console.WriteLine(screamer.playState);
+                    break;*/
+            }
+        }
+    }
+}

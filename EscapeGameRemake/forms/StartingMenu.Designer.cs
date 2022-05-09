a96779067a8bbb79d13143742416d17ab2d7f741
AriZox <arizoxfr@gmail.com>
2022-05-09 13:48:54 +0200

    fixed maximize box

--- EscapeGameRemake/forms/StartingMenu.Designer.cs
+++ EscapeGameRemake/forms/StartingMenu.Designer.cs
@@ -42,10 +42,10 @@ namespace EscapeGameRemake
             this.nose_honk_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.nose_honk_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.nose_honk_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.nose_honk_button.Location = new System.Drawing.Point(1021, 321);
-            this.nose_honk_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nose_honk_button.Location = new System.Drawing.Point(766, 261);
+            this.nose_honk_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nose_honk_button.Name = "nose_honk_button";
-            this.nose_honk_button.Size = new System.Drawing.Size(149, 76);
+            this.nose_honk_button.Size = new System.Drawing.Size(112, 62);
             this.nose_honk_button.TabIndex = 2;
             this.nose_honk_button.UseVisualStyleBackColor = false;
             this.nose_honk_button.Click += new System.EventHandler(this.nose_honk_button_Click);
@@ -58,10 +58,10 @@ namespace EscapeGameRemake
             this.play_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.play_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.play_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.play_button.Location = new System.Drawing.Point(251, 333);
-            this.play_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.play_button.Location = new System.Drawing.Point(188, 271);
+            this.play_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.play_button.Name = "play_button";
-            this.play_button.Size = new System.Drawing.Size(131, 52);
+            this.play_button.Size = new System.Drawing.Size(98, 42);
             this.play_button.TabIndex = 3;
             this.play_button.UseVisualStyleBackColor = false;
             this.play_button.Click += new System.EventHandler(this.play_button_Click);
@@ -75,10 +75,10 @@ namespace EscapeGameRemake
             this.credits_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.credits_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.credits_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.credits_button.Location = new System.Drawing.Point(251, 416);
-            this.credits_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.credits_button.Location = new System.Drawing.Point(188, 338);
+            this.credits_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.credits_button.Name = "credits_button";
-            this.credits_button.Size = new System.Drawing.Size(228, 52);
+            this.credits_button.Size = new System.Drawing.Size(171, 42);
             this.credits_button.TabIndex = 4;
             this.credits_button.UseVisualStyleBackColor = false;
             this.credits_button.Click += new System.EventHandler(this.credits_button_Click);
@@ -86,17 +86,18 @@ namespace EscapeGameRemake
             // 
             // StartingMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::EscapeGameRemake.Properties.Resources.StartingMenu_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1482, 753);
+            this.ClientSize = new System.Drawing.Size(1112, 612);
             this.Controls.Add(this.credits_button);
             this.Controls.Add(this.play_button);
             this.Controls.Add(this.nose_honk_button);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MinimizeBox = false;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
             this.Name = "StartingMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
